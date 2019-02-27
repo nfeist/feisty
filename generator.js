0adcfb7ff5050d774e4fd43a8c49301243849a58
nfeist <47568802+nfeist@users.noreply.github.com>
2019-02-27 13:47:33 -0700

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,11 +1,9 @@
 var n=1000000;
 var c=Math.random()*100;
-var v=Math.random()*10;
-
 var out=c;
+console.log("xValue, yValue");
 for(var i=0;i<n;i++){
-  out+=","+c;
+  out+=i+","+c+"\n";
   c+=(Math.random()*2)-1;
-  v+=(Math.random()*2)-1;
 }
 console.log(out);
